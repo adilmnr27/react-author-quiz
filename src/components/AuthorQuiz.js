f72d337067cc531481ee086fea00f42d6aa82595
adilmnr27 <adilmnr27@gmail.com>
2020-06-29 20:14:17 +0530

    Moved getting book data one level down from Author Quiz to Turn as it felt more appropriate

--- src/components/AuthorQuiz.js
+++ src/components/AuthorQuiz.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import booksList from '../utilities/mockDataForBooks.json';
 import Turn from './Turn';
-import {shuffle} from 'underscore'; //For Creating a random set of Authors
-import {sample} from 'underscore'; //For taking a random book
 export default function AuthorQuiz() {
 
     const header = "Author Quiz";
     const footer = "Footer";
-    var books = shuffledBooks(booksList.books);
-    var book = getRandomBook(books);
-
-    //Shuffling to Get 3 Random books. Calling the shuffle function from underscore library
-    function shuffledBooks(books){
-        return shuffle(books).slice(0,3);
-    }
-
-    /* returns a random book from the provided list */
-    function getRandomBook(books){
-        return sample(books);
-    }
 
     return (
         <div className="container  col-10 -1">
             <Header header={header}/>
-            <Turn name={book.name} books={books}  />
+            <Turn />
             <Footer footer={footer}/>
         </div>
       
--- src/components/Turn.js
+++ src/components/Turn.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import BookName from './BookName'
 import Author from './Author';
+import booksList from '../utilities/mockDataForBooks.json';
+import {shuffle} from 'underscore'; //For Creating a random set of Authors
+import {sample} from 'underscore'; //For taking a random book
+
 export default function Turn(props) {
+
+    var books = shuffledBooks(booksList.books);
+    var book = getRandomBook(books);
 
     /* Many people suggested to use map for it. But wanted to try this out */
     var authors = () => {
         let authors = [];
-        for (let i = 0; i < props.books.length; i++) {
+        for (let i = 0; i < books.length; i++) {
             /* key needs to provided for dynamic lists */
-            authors.push(<Author author={props.books[i].author} key={props.books[i].author} />)
+            authors.push(<Author author={books[i].author} key={books[i].author} />)
         }
         return authors;
     }
 
+    //Shuffling to Get 3 Random books. Calling the shuffle function from underscore library
+    function shuffledBooks(books){
+        return shuffle(books).slice(0,3);
+    }
+
+    /* returns a random book from the provided list */
+    function getRandomBook(books){
+        return sample(books);
+    }
 
     return (
         <div className="row turn col-10 -1" style={{ backgroundColor: "white" }}>
             <div className="col-4">
-               <h2><BookName name={props.name}/></h2>
+               <h2><BookName name={book.name}/></h2>
             </div>
             <div className="col-6">
                 <h2>{authors()}</h2>
